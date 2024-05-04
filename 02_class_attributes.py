7b2ded22cc77e6fffe8008a4123657eaea8d3eb6
Robert Vari <mail.robertvari@gmail.com>
2024-05-04 14:20:09 +0200

    update

---
+++ 02_class_attributes.py
@@ -0,0 +1,9 @@
+class Person:
+    # class attributes
+    name = "Balázs"
+    address = "Budapest"
+    email = "balazs@gmail.com"
+
+print(Person.name)
+print(Person.address)
+print(Person.email)
