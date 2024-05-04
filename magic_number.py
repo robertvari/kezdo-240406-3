c1e63af54fd13a7acffb9d60ac6fb3a3e108e98a
Robert Vari <mail.robertvari@gmail.com>
2024-05-04 18:05:34 +0200

    update

--- magic_number.py
+++ magic_number.py
@@ -2,6 +2,8 @@ import os, random
 
 class Game:
     def __init__(self):
+        self.__player = Player()
+        self.__computer = Computer()
         self.__clear_screen()
         self.__intro()
 
@@ -10,11 +12,13 @@ class Game:
 
     def __intro(self):
         print("*"*50, "MAGIC NUMBER GAME", "*"*50)
-
-        print(f"Hello Player Name! You have 0 credits to play.")
+        
+        self.__player.get_player_name()
+        
+        print(f"Hello {self.__player}! You have {self.__player.credits} credits to play.")
         print("If you can guess my number you win 10 credits, otherwise you loose 10 credits")
         print("I you have no more credits the game is over.")
-        print(f"I have number between 1 and 10. Can you guess it?")
+        print(f"I have number between {self.__computer.min_number} and {self.__computer.max_number}. Can you guess it?")
         print("-"*50)
 
 class Player:
@@ -23,6 +27,10 @@ class Player:
         self.__credits = 100
         self.__my_number = "0"
     
+    @property
+    def credits(self):
+        return self.__credits
+
     def think_a_number(self):
         self.__my_number = input("What is your guess? ")
 
@@ -42,7 +50,7 @@ class Player:
 
 class Computer:
     def __init__(self):
-        self.__min_number = 0
+        self.__min_number = 1
         self.__max_number = 10
         self.__my_number = "0"
 
@@ -50,12 +58,16 @@ class Computer:
     def my_number(self):
         return self.__my_number
 
+    @property
+    def min_number(self):
+        return self.__min_number
+    
+    @property
+    def max_number(self):
+        return self.__max_number
+
     def think_a_number(self):
         self.__my_number = str(random.randint(self.__min_number, self.__max_number))
 
 
-# Game()
-
-player = Player()
-player.get_player_name()
-player.report()
+Game()
