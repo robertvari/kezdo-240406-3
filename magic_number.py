55beee2cc582acabd3aca9e40188d03fa88ccb26
Robert Vari <mail.robertvari@gmail.com>
2024-05-04 17:41:13 +0200

    update

--- magic_number.py
+++ magic_number.py
@@ -10,10 +10,10 @@ class Game:
     def __intro(self):
         print("*"*50, "MAGIC NUMBER GAME", "*"*50)
 
-        print(f"Hello {self.player}! You have {self.player.credits} credits to play.")
+        print(f"Hello Player Name! You have 0 credits to play.")
         print("If you can guess my number you win 10 credits, otherwise you loose 10 credits")
         print("I you have no more credits the game is over.")
-        print(f"I have number between 1 and {self.computer.max_number}. Can you guess it?")
+        print(f"I have number between 1 and 10. Can you guess it?")
         print("-"*50)
 
 class Player:
