deffada8a181ac9a18ed4d758201fd80343a6b83
Robert Vari <mail.robertvari@gmail.com>
2024-05-05 17:00:51 +0200

    update

--- Blackjack/game_assets/players.py
+++ Blackjack/game_assets/players.py
@@ -9,10 +9,14 @@ class PlayerBASE:
         self.__playing = True
 
         # Then method calls
-        self.__create()
+        self._create()
 
-    def __create(self):
+    def _create(self):
         self.__credits = random.randint(10, 100)
+        self.__name = self.get_random_name()
+
+    def _set_name(self, new_name):
+        self.__name = new_name
 
     @staticmethod
     def get_random_name():
@@ -22,10 +26,14 @@ class PlayerBASE:
 
 
 class Player(PlayerBASE):
-    pass
+    def _create(self):
+        super()._create()  # call to BASE._create() and get random name and credits
+        result = input("What is your name? ")  # Get player name
+        self._set_name(result)  # call BASE._set_name() to set private __name
 
 class AIPlayer(PlayerBASE):
     pass
+
 
 if __name__ == "__main__":
     player = Player()
